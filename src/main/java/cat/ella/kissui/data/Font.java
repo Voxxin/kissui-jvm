fac55cb84af38a4a72d8657c70e86824358f242a
iris <60985521+KaiAF@users.noreply.github.com>
2025-06-04 18:06:32 -0500

    add font classes

--- src/main/java/cat/ella/kissui/data/Font.java
+++ src/main/java/cat/ella/kissui/data/Font.java
@@ -1,4 +1,56 @@
 package cat.ella.kissui.data;
 
-public class Font {
+public record Font(String path, float lineSpacing, float letterSpacing, FontFamily family, Font.Weight weight, boolean italic) {
+  public Font(String path, FontFamily family, boolean italic, Font.Weight weight) {
+	 this(path, 1.2F, 0F, family, weight, italic);
+  }
+
+  @Override
+  public String toString() {
+	 return "Font{" +
+				"path='" + path + '\'' +
+				", lineSpacing=" + lineSpacing +
+				", letterSpacing=" + letterSpacing +
+				", weight=" + weight +
+				", italic=" + italic +
+				'}';
+  }
+
+  public enum Weight {
+	 REGULAR(400, null),
+	 BOLD(700, null),
+
+	 MEDIUM(500, REGULAR),
+	 LIGHT(300, REGULAR),
+
+	 SEMI_BOLD(600, BOLD),
+	 EXTRA_BOLD(800, BOLD),
+	 BLACK(900, EXTRA_BOLD),
+
+	 EXTRA_LIGHT(200, LIGHT),
+	 THIN(100, EXTRA_LIGHT),
+	 ;
+
+	 public final int value;
+	 public final Weight weight;
+
+	 Weight(int value, Weight weight) {
+		this.value = value;
+		this.weight = weight;
+	 }
+
+	 public static Weight byWeight(int weight) {
+		return switch (weight / 100) {
+		  case 1 -> Weight.THIN;
+		  case 2 -> Weight.EXTRA_LIGHT;
+		  case 3 -> Weight.LIGHT;
+		  case 5 -> Weight.MEDIUM;
+		  case 6 -> Weight.SEMI_BOLD;
+		  case 7 -> Weight.BOLD;
+		  case 8 -> Weight.EXTRA_BOLD;
+		  case 9 -> Weight.BLACK;
+		  default -> Weight.REGULAR;
+		};
+	 }
+  }
 }
---
+++ src/main/java/cat/ella/kissui/data/FontFamily.java
@@ -0,0 +1,87 @@
+package cat.ella.kissui.data;
+
+import java.net.URI;
+import java.net.URL;
+
+public class FontFamily {
+  private final String name;
+  private final URI path;
+
+  public FontFamily(String name, String path) {
+	 this.name = name;
+	 this.path = URI.create((path.startsWith("/") ? "" : "/") + path);
+  }
+
+  public Font get(Font.Weight weight, boolean italic) {
+	 return switch (weight) {
+		case Font.Weight.THIN -> thin(italic);
+		case Font.Weight.EXTRA_LIGHT -> extraLight(italic);
+		case Font.Weight.LIGHT -> light(italic);
+		case Font.Weight.REGULAR -> regular(italic);
+		case Font.Weight.MEDIUM -> medium(italic);
+		case Font.Weight.SEMI_BOLD -> semiBold(italic);
+		case Font.Weight.BOLD -> bold(italic);
+		case Font.Weight.EXTRA_BOLD -> extraBold(italic);
+		case Font.Weight.BLACK -> black(italic);
+	 };
+  }
+
+  public Font regular(boolean italic) {
+	 return this.load(Font.Weight.REGULAR, italic);
+  }
+
+  public Font bold(boolean italic) {
+	 return this.load(Font.Weight.BOLD, italic);
+  }
+
+  public Font medium(boolean italic) {
+	 return this.load(Font.Weight.MEDIUM, italic);
+  }
+
+  public Font light(boolean italic) {
+	 return this.load(Font.Weight.LIGHT, italic);
+  }
+
+  public Font semiBold(boolean italic) {
+	 return this.load(Font.Weight.SEMI_BOLD, italic);
+  }
+
+  public Font extraBold(boolean italic) {
+	 return this.load(Font.Weight.EXTRA_BOLD, italic);
+  }
+
+  public Font black(boolean italic) {
+	 return this.load(Font.Weight.BLACK, italic);
+  }
+
+  public Font extraLight(boolean italic) {
+	 return this.load(Font.Weight.EXTRA_LIGHT, italic);
+  }
+
+  public Font thin(boolean italic) {
+	 return this.load(Font.Weight.THIN, italic);
+  }
+
+  private String getStyle(Font.Weight weight, boolean italic) {
+	 if (italic) {
+		if (weight == Font.Weight.REGULAR) return "Italic";
+		return weight + "Italic";
+	 }
+
+	 return "" + weight;
+  }
+
+  private Font load(Font.Weight weight, boolean italic) {
+	 String style = getStyle(weight, italic);
+	 String name = String.format("%s-%s.ttf", this.name, style);
+	 String filePath = this.path.resolve(name).toString();
+
+	 URL url = getClass().getResource(filePath.toLowerCase());
+	 if (url != null) {
+		return new Font(filePath, this, italic, weight);
+	 } else {
+		// todo: add a default font
+		return null;
+	 }
+  }
+}
